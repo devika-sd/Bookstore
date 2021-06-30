9e0d0a5f8db21e261226defa8554373bc271c62f
devika-sd <devikajeevaraj1310@gmail.com>
2021-06-30 19:21:58 +0530

    email-regex-changed

--- models/orders.js
+++ models/orders.js
@@ -10,7 +10,7 @@ const OrdersSchema = new schema({
         type: String,
         trim: true,
         required: [true, 'Please provide a Email'],
-        match: [/^\w+([\.-]?\w+)@\w+([\.-]?\w+)(\.\w{2,3})+$/, 'Please provide a valid Email Address']
+        match: [/^[a-zA-Z0-9+_.-]+@[a-zA-Z0-9.-]+$/, 'Please provide a valid Email Address']
     },
     amount: {
         type: Number,
